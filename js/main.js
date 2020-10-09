c02f963ed5d68b6c35d578315804fe11dec2bb9b
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2020-10-09 12:18:59 +0300

    Проект иконки на карте 1
    Проект иконки на карте 1

--- js/main.js
+++ js/main.js
@@ -29,9 +29,9 @@ const selectFromArray = (array) => {
   return array[element];
 };
 
-const countOfAds = 1;
+const countOfAds = 8;
 const avatarImg = [1, 8];
-const titleText = [`Природные парки, заповедники`, `Долина Гейзеров, Камчатка`, `Воробьевы горы`,`Патриаршие пруды`,`Чистые пруды`,`Парк «Коломенское»`,`Нескучный сад`];
+const titleText = [`Природные парки, заповедники`, `Долина Гейзеров, Камчатка`, `Воробьевы горы`, `Патриаршие пруды`, `Чистые пруды`, `Парк «Коломенское»`, `Нескучный сад`];
 const address = ``;
 const price = [100000, 5000];
 const type = [`palace`, `flat`, `house`, `bungalow`];
@@ -40,9 +40,9 @@ const guests = [10, 1];
 const checkin = [`12:00`, `13:00`, `14:00`];
 const checkout = [`12:00`, `13:00`, `14:00`];
 const features = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const desсriptipon = [`«Остров Мечты» станет крайне привлекательной городской туристической достопримечательностью и встанет в один ряд с Кремлем, Парком Горького и ВДНХ`,`Усадьба Царицыно – единственное место в Москве, которое может похвастаться «Поющим световым фонтаном».`,`Музеон – место творческих людей. Здесь постоянно проводят выставки художников, музыкальные фестивали и видеоперфомансы. Сюда часто приходят за вдохновением и идеями.`];
+const desсriptipon = [`«Остров Мечты» станет крайне привлекательной городской туристической достопримечательностью и встанет в один ряд с Кремлем, Парком Горького и ВДНХ`, `Усадьба Царицыно – единственное место в Москве, которое может похвастаться «Поющим световым фонтаном».`, `Музеон – место творческих людей. Здесь постоянно проводят выставки художников, музыкальные фестивали и видеоперфомансы. Сюда часто приходят за вдохновением и идеями.`];
 const photos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
-const blockSze = 300;
+const blockSze = [100, 1000];
 const locX = blockSze;
 const locY = [130, 630];
 
@@ -66,7 +66,7 @@ const getAdsOject = () => {
       photos: selectFromArray(photos)
     },
     location: {
-      x: getRandomInt(locX),
+      x: minMaxNumber(locX),
       y: minMaxNumber(locY)
     }
   };
@@ -84,6 +84,36 @@ const getlistAds = (countAds) => {
 
 const newAdsItem = getlistAds(countOfAds);
 
+document.querySelector(`.map`).classList.remove(`map--faded`);
+
+const renderOfAds = (arrayAds) => {
+  const mapPins = document.querySelector(`.map__pins`);
+  const fragment = document.createDocumentFragment();
+  const mapPinTamplate = document.querySelector(`#pin`).content;
+
+  for (let i = 0; i < arrayAds.length; i++) {
+
+    const mapPinItem = mapPinTamplate.querySelector(`.map__pin`).cloneNode(true);
+    const pinIcon = mapPinItem.querySelector('img');
+
+    pinIcon.src = arrayAds[i].author.avatar;
+    pinIcon.alt = arrayAds[i].offer.title;
+
+    mapPinItem.style.left = arrayAds[i].location.x + (pinIcon.width / 2) + `px`;
+    mapPinItem.style.top = arrayAds[i].location.y + pinIcon.height + `px`;
+
+
+    arrayAds[i].offer.address = arrayAds[i].location.x + `, ` + arrayAds[0].location.y;
+
+    fragment.appendChild(mapPinItem);
+
+  }
+
+  mapPins.appendChild(fragment);
+
+};
+
+renderOfAds(newAdsItem);
 
 
 
